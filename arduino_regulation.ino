0e19fc68223aab353bf728fe7743ae2cf6ad405d
Tomáš Moravec <moravec-tomas@seznam.cz>
2018-01-08 22:12:04 +0100

    Added project

---
+++ arduino_regulation.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include <AM2320.h>
+#include <LiquidCrystal.h>
+
+void setup()
+{
+
+  /* add setup code here */
+	
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
